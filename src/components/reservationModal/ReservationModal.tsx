1fae1e61ea1f12a2930daed63d4577bb643a3484
Bohdan <bohdan.krav4enko@gmail.com>
2026-07-10 05:48:43 +0300

    feat: complete all pending tasks

--- src/components/reservationModal/ReservationModal.tsx
+++ src/components/reservationModal/ReservationModal.tsx
@@ -42,7 +42,10 @@ export const ReservationModal = ({
         await emailjs.send(
             import.meta.env.VITE_EMAILJS_SERVICE_ID,
             import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-            data,
+            {
+                ...data,
+                message: data.message || "No special requests",
+            },
             import.meta.env.VITE_EMAILJS_PUBLIC_KEY
         );
 
